8886aa142a8977df29d00058638b3e4721c5546b
Andrew Wong <wongandrew97@gmail.com>
2017-01-10 01:55:46 -0500

    Added Iron Router

--- tearn/imports/startup/client/routes.js
+++ tearn/imports/startup/client/routes.js
@@ -1,21 +1,14 @@
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
-
 // Import needed templates
-import '../../ui/layouts/body/body.js';
+import '../../ui/layouts/body.html';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
 
-// Set up all routes in the app
-FlowRouter.route('/', {
-  name: 'App.home',
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_home' });
-  },
+Router.configure({
+  layoutTemplate: 'appbody',
+  notFoundTemplate: 'not-found'
 });
 
-FlowRouter.notFound = {
-  action() {
-    BlazeLayout.render('App_body', { main: 'App_notFound' });
-  },
-};
+Router.route('/', {
+  name: 'home',
+  template: 'home'
+});

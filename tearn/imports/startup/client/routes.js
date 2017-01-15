62609504acb62bb6b7623fe00a7065d8d0ab21d9
Andrew Wong <wongandrew97@gmail.com>
2017-01-15 02:15:13 -0500

    Set up routes and homepage

--- tearn/imports/startup/client/routes.js
+++ tearn/imports/startup/client/routes.js
@@ -1,14 +1,14 @@
 // Import needed templates
-import '../../ui/layouts/body.html';
-import '../../ui/pages/home/home.js';
-import '../../ui/pages/not-found/not-found.js';
+import '../../ui/layouts/main.html';
+import '../../ui/components/not-found/not-found.html';
+import '../../ui/components/nav/nav.html';
+import '../../ui/components/home/home.html';
 
 Router.configure({
-  layoutTemplate: 'appbody',
+  layoutTemplate: 'main',
   notFoundTemplate: 'not-found'
 });
 
 Router.route('/', {
-  name: 'home',
-  template: 'home'
+  name: 'home'
 });

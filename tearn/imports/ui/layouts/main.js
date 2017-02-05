6d72ee9b3af440bdec63a84c3a0a8beb70738c00
Andrew Wong <wongandrew97@gmail.com>
2017-02-05 12:34:46 -0500

    Add landing page for gh-pages

--- tearn/imports/ui/layouts/main.js
+++ tearn/imports/ui/layouts/main.js
@@ -1,3 +1,3 @@
 import './main.html';
 import '../stylesheets/font-awesome.min.css';
-import '../stylesheets/bulma.css';
+import '../stylesheets/bulma.css'; // cannot import sass that is why we import css

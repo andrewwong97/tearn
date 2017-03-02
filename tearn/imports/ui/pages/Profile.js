3ec76517398695205b08ffef44aa0229594afdcb
Jason Zhang <jz1242@gmail.com>
2017-03-02 13:45:37 -0500

    Set delay for route to load name faster, added jobview

--- tearn/imports/ui/pages/Profile.js
+++ tearn/imports/ui/pages/Profile.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Image, Row, Col, Nav, NavItem } from 'react-bootstrap';
 import { Link } from 'react-router';
 
+
 export default class Profile extends React.Component {
   constructor(props) {
     super(props);
@@ -14,20 +15,21 @@ export default class Profile extends React.Component {
   getUsername() {
     const user = Meteor.user();
     const name = user && user.profile ? user.profile.name : '';
-    return user ? `${name.first} ${name.last}` : 'My Profile';
-  }
 
+    return name.first + ' '+ name.last;
+  }
    handleSelect(selectedKey) {
       this.setState({
         activeKey: selectedKey
       });
    }
 
-  render() {
+    render() {
+    console.log(this.getUsername() == 'undefined undefined');
     return (
       <div className="Profile">
         <div className="profile-container">
-          <h1>{this.getUsername()}</h1>
+          <h1>{this.getUsername()}</h1>  
           <Nav className="profile-nav" bsStyle="pills" activeKey={this.state.activeKey} onSelect={this.handleSelect.bind(this)}>
             <NavItem bsStyle="btn btn-success" eventKey={1}>Teacher</NavItem>
             <NavItem bsStyle="btn btn-success" eventKey={2}>Learner</NavItem>

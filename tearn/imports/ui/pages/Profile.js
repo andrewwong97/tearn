ed130237241cb07db33680959e4f4a9329701522
Andrew Wong <wongandrew97@gmail.com>
2017-03-01 22:47:19 -0500

    Profile nav bar flex align

--- tearn/imports/ui/pages/Profile.js
+++ tearn/imports/ui/pages/Profile.js
@@ -7,35 +7,29 @@ import { Link } from 'react-router';
 export default class Profile extends React.Component {
   constructor(state) {
     super(state);
-    this.handleSelect = this.handleSelect;
-           this.state = {
-            activeKey: 1
-        };
-
+    this.state = {
+      activeKey: 1
+    };
   }
 
    handleSelect(selectedKey) {
-      this.setState({
-            activeKey: selectedKey
-        });
+    this.setState({
+        activeKey: selectedKey
+    });
    }
 
   render() {
     return (
       <div className="Profile">
-        <div className="container">
+        <div className="profile-container">
           <h1>My Profile</h1>
           <Nav className="profile-nav" bsStyle="pills" activeKey={this.state.activeKey} onSelect={this.handleSelect.bind(this)}>
             <NavItem bsStyle="btn-success" eventKey={1}>Teacher</NavItem>
             <NavItem bsStyle="btn-success" eventKey={2} title="Item">Learner</NavItem>
           </Nav>
-          <Row>
-            <Col xs={6} md={4}>
-              <div className="profile-img"></div>
-            </Col>
-          </Row>
           <br></br>
         </div>
+        <div className="profile-img"></div>
       </div>
     );
   }
